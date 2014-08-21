d89a25f00e6c0bd8c8fbe4c7e2ea66fc65018d94
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-21 18:30:13 +0700

    report: report folder size

--- report.pl
+++ report.pl
@@ -11,7 +11,36 @@ use JSON;
 my $jS0n = do { local $/; <STDIN> };
 my $json = decode_json( $jS0n );
 my $folders = $json->{"folders"};
+
 for ( keys @{$folders} ) {
   my $d = $folders->[$_];
-  printf "key: %s name: %s\n", $d->{"secret"}, $d->{"name"};
+  printf "key: %s name: %s size: %s\n",
+    $d->{"secret"}, $d->{"name"}, get_filesize_str($d->{"size"});
+}
+
+# http://www.use-strict.de/perl-file-size-in-a-human-readable-format.html
+sub get_filesize_str
+{
+    my $size = shift;
+
+    if ($size > 1099511627776)  #   TiB: 1024 GiB
+    {
+        return sprintf("%.2fTiB", $size / 1099511627776);
+    }
+    elsif ($size > 1073741824)  #   GiB: 1024 MiB
+    {
+        return sprintf("%.2fGiB", $size / 1073741824);
+    }
+    elsif ($size > 1048576)     #   MiB: 1024 KiB
+    {
+        return sprintf("%.2fMiB", $size / 1048576);
+    }
+    elsif ($size > 1024)        #   KiB: 1024 B
+    {
+        return sprintf("%.2fKiB", $size / 1024);
+    }
+    else                          #   bytes
+    {
+        return "${size}B" . ($size <= 1 ? "" : "s");
+    }
 }
